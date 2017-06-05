5bf805674dec930206f92c2fce20da22d97c6db6
MeltzerF <slobozhenuke@gmail.com>
2017-06-05 11:42:21 +0300

    DI

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -4,12 +4,13 @@ class Logger {
     private LoggerFilter filter;
     private LoggerSaver saver;
 
+    // DI - dependency injection (реализация ложится на выхывающую сторону)
     Logger(LoggerFilter filter, LoggerSaver saver) {
         this.filter = filter;
         this.saver = saver;
     }
 
-    void log(String message) {
+    public void log(String message) {
         if (filter.filter(message))
             saver.save(message);
     }
