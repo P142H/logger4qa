2b6c5386c40ee36286d9376c99f1aaf63232c36a
Sergey matyukhin <serg.m83@gmail.com>
2017-06-09 11:46:21 +0300

    Update Logger.java

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -3,15 +3,17 @@ class Logger {
 
     private LoggerFilter filter;
     private LoggerSaver saver;
+    private LoggerFormatter formatter;
 
     // DI - dependency injection (реализация ложится на выхывающую сторону)
-    Logger(LoggerFilter filter, LoggerSaver saver) {
+    Logger(LoggerFilter filter, LoggerSaver saver, LoggerFormatter formatter) {
         this.filter = filter;
         this.saver = saver;
+        this.formatter = formatter;
     }
 
     public void log(String message) {
         if (filter.filter(message))
-            saver.save(message);
+            saver.save(formatter.format(message));
     }
 }
